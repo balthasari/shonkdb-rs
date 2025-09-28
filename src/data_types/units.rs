b5466d121536599de0783f7cd1fc80f2ba2b570a
balthasar <balthasar.richters@gmail.com>
2025-09-28 21:34:39 +0200

    metric du worky worky

--- src/data_types/units.rs
+++ src/data_types/units.rs
@@ -1,13 +1,20 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone)]
-struct MetricValue {
+#[derive(Serialize, Deserialize, Clone, Copy)]
+pub struct MetricValue {
     unit: MetricUnit,
     absolute: f32,
     prefix: MetricPrefix,
 }
 impl MetricValue {
-    fn convert_to(self, new_metric_prefix: MetricPrefix) -> Self {
+    pub fn new(numerical_absolute_value: f32, prefix: MetricPrefix, unit: MetricUnit) -> Self {
+        Self {
+            absolute: numerical_absolute_value,
+            unit: unit,
+            prefix: prefix,
+        }
+    }
+    pub fn convert_to(self, new_metric_prefix: MetricPrefix) -> Self {
         let conversion_scale: f32 = self.prefix.to(new_metric_prefix.clone());
         Self {
             unit: self.unit,
@@ -16,14 +23,38 @@ impl MetricValue {
         }
     }
 }
-#[derive(Serialize, Deserialize, Clone)]
-enum MetricUnit {
+
+impl ToString for MetricValue {
+    fn to_string(&self) -> String {
+        format!(
+            "{} {}{}",
+            self.absolute.to_string(),
+            self.prefix.to_string(),
+            self.unit.to_string()
+        )
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy)]
+pub enum MetricUnit {
     Meter,
     Seconds,
     Coulomb,
 }
-#[derive(Serialize, Deserialize, Clone)]
-enum MetricPrefix {
+
+impl ToString for MetricUnit {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Meter => "m",
+            Self::Coulomb => "C",
+            Self::Seconds => "s",
+            _ => "unknown",
+        }
+        .to_owned()
+    }
+}
+#[derive(Serialize, Deserialize, Clone, Copy)]
+pub enum MetricPrefix {
     quecto = -30,
     ronto = -27,
     yocto = -24,
@@ -57,6 +88,40 @@ impl MetricPrefix {
     }
 
     fn conversion(from_mpref: MetricPrefix, to_mpref: MetricPrefix) -> f32 {
-        f32::powf(10.0, (to_mpref as usize - from_mpref as usize) as f32)
+        f32::powf(10.0, (from_mpref as isize - to_mpref as isize) as f32)
+    }
+}
+
+impl ToString for MetricPrefix {
+    fn to_string(&self) -> String {
+        match self {
+            Self::quecto => "q",
+            Self::ronto => "r",
+            Self::yocto => "y",
+            Self::zepto => "z",
+            Self::atto => "a",
+            Self::femto => "f",
+            Self::pico => "p",
+            Self::nano => "n",
+            Self::micro => "μ",
+            Self::milli => "m",
+            Self::centi => "c",
+            Self::deci => "d",
+            Self::si => "",
+            Self::deca => "da",
+            Self::hecto => "h",
+            Self::kilo => "k",
+            Self::mega => "M",
+            Self::giga => "G",
+            Self::tera => "T",
+            Self::peta => "P",
+            Self::exa => "E",
+            Self::zetta => "Z",
+            Self::yotta => "Y",
+            Self::rotta => "R",
+            Self::quetta => "Q",
+            _ => "unkown",
+        }
+        .to_owned()
     }
 }
